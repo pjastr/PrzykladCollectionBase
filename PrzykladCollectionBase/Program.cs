8a8f42c3a2ed26ea5169071f1037780da7b53e80
pjastr <24464854+pjastr@users.noreply.github.com>
2018-01-15 15:30:23 +0100

    przeslanie plików

---
+++ PrzykladCollectionBase/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrzykladCollectionBase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WlasnaKolekcja wk = new WlasnaKolekcja();
+            wk.Add("sss");
+            wk.Add(455);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PrzykladCollectionBase/WlasnaKolekcja.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrzykladCollectionBase
+{
+    class WlasnaKolekcja:CollectionBase
+    {
+        protected override void OnInsert(int index, Object value)
+        {
+            Console.WriteLine("Dodano {1} na pozycji {0}", index, value);
+        }
+
+        public int Add(object value)
+        {
+            return (List.Add(value));
+        }
+    }
+}
